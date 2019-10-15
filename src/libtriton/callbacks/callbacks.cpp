c7e6dc1ccca8161c3ec91ca5ced7e0e17766ab43
Jonathan Salwan <jonathan.salwan@gmail.com>
2019-10-15 11:08:47 +0200

    Rename bool to its enum

--- src/libtriton/callbacks/callbacks.cpp
+++ src/libtriton/callbacks/callbacks.cpp
@@ -128,7 +128,7 @@ namespace triton {
             this->mload = true;
             function(this->api, mem);
             if (mem.getLeaAst() != nullptr)
-                this->api.getSymbolicEngine()->initLeaAst(const_cast<triton::arch::MemoryAccess&>(mem), true);
+                this->api.getSymbolicEngine()->initLeaAst(const_cast<triton::arch::MemoryAccess&>(mem), triton::arch::FORCE_MEMORY_INITIALIZATION);
             this->mload = false;
           }
 
